e9d48854e14cb13ccde967400f1d02961c011e8d
“sw353552” <swathidurai94@gmail.com>
2025-04-26 00:05:47 +0530

    fixed errors intest file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Products header and Add Products button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const heading = screen.getByText(/products/i);
+  expect(heading).toBeInTheDocument();
+
+  const addButton = screen.getByRole('button', { name: /add products/i });
+  expect(addButton).toBeInTheDocument();
+
+  const emptyText = screen.getByText(/no products found/i);
+  expect(emptyText).toBeInTheDocument();
 });
